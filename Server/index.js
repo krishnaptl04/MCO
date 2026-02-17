4bba69101e029aaed5c1bc92c51d795d523d6b20
krishna <krishnaptl0414@gmail.com>
2026-02-17 17:12:05 +0530

    first Commit

---
+++ Server/index.js
@@ -0,0 +1,23 @@
+import express from "express"
+import mongoose from "mongoose"
+import bodyParser from "body-parser"
+import dotenv from "dotenv"
+
+const app = express();
+app.use(bodyParser.json());
+dotenv.config();
+
+const PORT = process.env.PORT || 7000;
+const MONGOURL = process.env.MONGO_URL;
+
+mangoose
+    .connect(MONGOURL)
+    .then(() => {
+        console.log("DB connected sucessfully.");
+        app.listen(PORT, () => {
+            console.log(`Server is running on port:${PORT}`)
+
+        });
+
+    })
+    .catch((error) => console.log(error));
