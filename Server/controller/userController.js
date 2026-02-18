2eab3fea4800e09b76a261ae838a4c487a861c91
krishna <krishnaptl0414@gmail.com>
2026-02-18 13:37:02 +0530

    update the user by id

--- Server/controller/userController.js
+++ Server/controller/userController.js
@@ -44,3 +44,20 @@ export const getUserById = async (req, res) => {
         res.status(500).json({ errorMessage: error.message });
     }
 };
+
+export const update = async (req, res) => {
+
+    try {
+        const id = req.params.id;
+        const userExist = await User.findById(id);
+        if (!userExist) {
+            return res.status(404).json({ message: "user not found." })
+        }
+        const updatedData = await User.findByIdAndUpdate(id, req.body, {
+            new: true
+        })
+        res.status(200).json(updatedData);
+    } catch (error) {
+        res.status(500).json({ errorMessage: error.message });
+    }
+}
--- Server/routes/userRoute.js
+++ Server/routes/userRoute.js
@@ -1,11 +1,11 @@
 import express from "express"
 
-import { create, getAllUser, getUserById } from "../controller/userController.js"
+import { create, getAllUser, getUserById, update } from "../controller/userController.js"
 
 const route = express.Router();
 
 route.post("/user", create);
 route.get("/users", getAllUser);
 route.get("/user/:id", getUserById);
-
+route.put("/update/user/:id", update)
 export default route;
