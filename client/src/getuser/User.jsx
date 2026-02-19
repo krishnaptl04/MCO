37958e8e5d37345fd97c5fcf8e42d6375346c56b
krishna <krishnaptl0414@gmail.com>
2026-02-19 10:18:48 +0530

    create User table using Bootstrap

--- client/src/getuser/User.jsx
+++ client/src/getuser/User.jsx
@@ -1,7 +1,34 @@
 import react from "react";
+import "./User.css";
 
 const User = () => {
-  return <div>User Table</div>;
+  return (
+    <div className="userTable">
+      <button type="button" class="btn btn-primary">
+        Add User
+      </button>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th scope="col">S.NO</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Address</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Joun</td>
+            <td>joun@gmail.com</td>
+            <td>Canada</td>
+            <td>Update | Delete</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default User;
