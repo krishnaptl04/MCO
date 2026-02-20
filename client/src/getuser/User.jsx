17684aad09990f11b07e816980a60831f9623594
krishna <krishnaptl0414@gmail.com>
2026-02-20 10:28:02 +0530

    set up react-host-toast

--- client/src/getuser/User.jsx
+++ client/src/getuser/User.jsx
@@ -1,4 +1,4 @@
-import react, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./User.css";
 import axios from "axios";
 import { Link } from "react-router-dom";
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Toaster } from "react-hot-toast"
 
 import App from './App';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Toaster />
   </React.StrictMode>
 );
 
