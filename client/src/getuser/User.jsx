3fdb8547327b8b885d3b645ec7177d4477ee6194
krishna <krishnaptl0414@gmail.com>
2026-02-20 12:59:02 +0530

    Delete User data & check how app is work

--- client/src/getuser/User.jsx
+++ client/src/getuser/User.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./User.css";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import toast from "react-hot-toast";
 
 const User = () => {
   const [users, setUsers] = useState([]);
@@ -17,46 +18,71 @@ const User = () => {
     fetchData();
   }, []);
 
+  const deleteUser = async (userId) => {
+    await axios
+      .delete(`http://localhost:8000/api/delete/user/${userId}`)
+      .then((response) => {
+        setUsers((prevUser) => prevUser.filter((user) => user._id !== userId));
+        toast.success(response.data.message, { position: "top-right" });
+      })
+
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="userTable">
       <Link to="/add" type="button" class="btn btn-primary">
         Add User <i class="fa-solid fa-user-plus"></i>
       </Link>
-      <table className="table table-bordered">
-        <thead>
-          <tr>
-            <th scope="col">S.NO</th>
-            <th scope="col">Name</th>
-            <th scope="col">Email</th>
-            <th scope="col">Address</th>
-            <th scope="col">Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user, index) => {
-            return (
-              <tr>
-                <td>{index + 1}</td>
-                <td>{user.name}</td>
-                <td>{user.email}</td>
-                <td>{user.address}</td>
-                <td className="actionButtons">
-                  <Link
-                    to={`/update/` + user._id}
-                    type="button"
-                    class="btn btn-info"
-                  >
-                    <i class="fa-regular fa-pen-to-square"></i>
-                  </Link>
-                  <button type="button" class="btn btn-danger">
-                    <i class="fa-solid fa-trash"></i>
-                  </button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+
+      {users.length === 0 ? (
+        <div className="noData">
+          <h3>No Data To Display</h3>
+          <p>Please add New User</p>
+        </div>
+      ) : (
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th scope="col">S.NO</th>
+              <th scope="col">Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Address</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => {
+              return (
+                <tr>
+                  <td>{index + 1}</td>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>{user.address}</td>
+                  <td className="actionButtons">
+                    <Link
+                      to={`/update/` + user._id}
+                      type="button"
+                      class="btn btn-info"
+                    >
+                      <i class="fa-regular fa-pen-to-square"></i>
+                    </Link>
+                    <button
+                      onClick={() => deleteUser(user._id)}
+                      type="button"
+                      class="btn btn-danger"
+                    >
+                      <i class="fa-solid fa-trash"></i>
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
